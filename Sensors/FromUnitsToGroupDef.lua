58d9b00189b8fca4930d37a48eec776aac0f4c12
MajdaTurinska <majda.turinska@email.cz>
2025-04-12 15:11:25 +0200

    upraveno do stavu na miroboard

---
+++ Sensors/FromUnitsToGroupDef.lua
@@ -0,0 +1,28 @@
+local sensorInfo = {
+	name = "FromUnitsToGroupDef",
+	desc = "Flip keys and values of units table",
+	author = "PepeAmpere_Majda",
+	date = "2025-04-12",
+	license = "MIT",
+}
+
+local EVAL_PERIOD_DEFAULT = -1 -- instatnt, no caching
+
+function getInfo()
+	return {
+		period = EVAL_PERIOD_DEFAULT 
+	}
+end
+
+-- @description return stuctured description of the formation 
+-- @argument listOfUnits [array]
+return function(listOfUnits)
+	local groupDefinition = {}
+	
+	for i=1, #listOfUnits do
+		local unitID = listOfUnits[i] -- get unitID
+		groupDefinition[unitID]= i
+	end
+	
+	return groupDefinition
+end
